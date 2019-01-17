45130a9ef2972c4ae3996f328ee459c7a82b563b
parinitamanasa <way2manasa@gmail.com>
2019-01-17 22:38:09 +0530

    Update ApplicationRunner.java

--- musixApplicationmySql/src/main/java/com/stackroute/configuration/ApplicationRunner.java
+++ musixApplicationmySql/src/main/java/com/stackroute/configuration/ApplicationRunner.java
@@ -1,22 +1,22 @@
 package com.stackroute.configuration;
 
-//import com.stackroute.domain.Track;
-//import com.stackroute.repository.TrackRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ApplicationRunner implements CommandLineRunner {
-//    @Autowired
-//    private TrackRepository trackRepository;
-//    @Override
-//    public void run(String... args) throws Exception {
-//        seedStart();
-//    }
-//    //This method adds seed data into the database
-//    public void seedStart(){
-//        trackRepository.save(new Track("track1","comment1"));
-//        trackRepository.save(new Track("track2","comment2"));
-//    }
-//}
+import com.stackroute.domain.Track;
+import com.stackroute.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationRunner implements CommandLineRunner {
+   @Autowired
+   private TrackRepository trackRepository;
+   @Override
+   public void run(String... args) throws Exception {
+       seedStart();
+   }
+   //This method adds seed data into the database
+   public void seedStart(){
+       trackRepository.save(new Track("track1","comment1"));
+       trackRepository.save(new Track("track2","comment2"));
+   }
+}
